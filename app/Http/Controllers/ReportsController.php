f7ba22a1ccb90f84610bdecd010321d7d7e19ed9
shabanelmansy <shaban_elmansy@yahoo.com>
2016-10-21 17:15:56 +0400

    report arabic font

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -632,7 +632,7 @@ class ReportsController extends Controller
 
                 
                 $text_head = 'كشف حضور والانصراف لبرنامج';
-                $section->addText($text_head, $fontStyleName, $paragraphStyleName);
+                $section->addText($text_head, array('name' => 'Andalus','bold' => false, 'size' => 16, 'allCaps' => true ,'alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER,'rtl' => true), $paragraphStyleName);
                 $section->addText( $course->title , $fontStyleName, $paragraphStyleName);
                 $section->addText( $course->instructor , $fontStyleName, $paragraphStyleName);
 
