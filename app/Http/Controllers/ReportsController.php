2e5a0b94413f9e0250f8095850a261ff115cff38
shabanelmansy <shaban_elmansy@yahoo.com>
2016-10-21 14:44:31 +0400

    report arabic font

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -658,8 +658,8 @@ class ReportsController extends Controller
                 $table->addCell(1000, $fancyTableCellStyle)->addText('   /     /    ', $fancyTableFontStyle);
                 $table->addCell(1000, $fancyTableCellStyle)->addText('   /     /    ', $fancyTableFontStyle);
                 $table->addCell(1000, $fancyTableCellStyle)->addText('   /     /    ', $fancyTableFontStyle);
-                $table->addCell(4000, $fancyTableCellStyle)->addText('الاسم', null, $fancyTableFontStyle); 
-                $table->addCell(600, $fancyTableCellStyle)->addText('الرقم', $fancyTableFontStyle);
+                $table->addCell(4000, $fancyTableCellStyle)->addText('الاسم', null , $fancyTableFontStyle); 
+                $table->addCell(600, $fancyTableCellStyle)->addText('الرقم', null ,$fancyTableFontStyle);
 
 
                 foreach($students as $student)
