c68c35b624f4c8aadf08905d14ca03124c86b1aa
shabanelmansy <shaban_elmansy@yahoo.com>
2016-10-21 17:26:09 +0400

    report arabic font

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -676,7 +676,7 @@ class ReportsController extends Controller
                     $table->addCell(1000,$fancyTableCellStyle)->addText("");
                     $table->addCell(1000,$fancyTableCellStyle)->addText("");
                     $table->addCell(1000,$fancyTableCellStyle)->addText("");
-                    $table->addCell(600,$fancyTableCellStyle)->addText($student->name_ar , null ,  $fancyTableFontStyle);
+                    $table->addCell(600,$fancyTableCellStyle)->addText($student->name_ar , array('rtl'=>true) ,  $fancyTableFontStyle);
                     $table->addCell(400,$fancyTableCellStyle)->addText($student->id , null , $fancyTableFontStyle);
 
                 }
